bc8cdc99b03d480298e7432adee7e4197cf50a42
Ramiro-Davila <150105216+Ramiro-Davila@users.noreply.github.com>
2024-08-30 21:15:26 -0600

    Create index.php

---
+++ pagina/pagina-web/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto Integrado</title>
+    <link rel="stylesheet" href="../css/style.css">    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="../img/E_SDG_Icons_NoText-04.jpg" alt="">
+            <a class="title" href="">proyecto integrador</a>
+        </div>
+        <div class="navbar">
+            <a href="#">Inicio</a>
+            <a href="#">Contenido</a>
+            <a href="#">Perfiles</a>
+            <a href="#">Contactos</a>
+        </div>
+       <a href="" class="boton">Inicia sesion</a>
+    </header>
+    <div class="fondo">
+        <div class="header-content">
+            <h2>Disminucion de desigual y educacion de calidad</h2>
+            <div class="line"></div>
+            <a href="" class="ctn">Saber mas</a>
+        </div>
+    </div>
+    <section>
+        <h2 class="title">Objetivos de la ONG</h2>
+        <div class="info-content">
+            <img src="../img/S_SDG_Icons-01-10-1.jpg" alt="" class="picture__img">
+            <div class="info">
+                <h2 class="subtitle">Disminucion de desigualdad</h2>
+                <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, doloribus obcaecati. Molestiae earum nemo ad illo porro, corporis amet enim officiis necessitatibus similique magnam! Vero ea sed hic rerum? Non?</p>
+            </div> 
+        </div>
+        <div class="info-content">
+            <img src="../img/S_SDG_Icons-01-04.jpg" alt="" class="picture__img">
+            <div class="info">
+                <h2 class="subtitle">Educacion de calidad</h2>
+                <p class="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, doloribus obcaecati. Molestiae earum nemo ad illo porro, corporis amet enim officiis necessitatibus similique magnam! Vero ea sed hic rerum? Non?</p>
+            </div> 
+        </div>
+    </section>
+   
+    <section class="programas">
+        <h2 class="title">Programas de la ONG para la disminucion de desigualdad y educacion de calidad</h2>
+        <div class="content">
+            <div class="content-prog">
+                <img src="../img/S_SDG_Icons-01-10-1.jpg" alt="" class="prog-img">
+                <h3 class="subtitle-prog">tema de programa</h3>
+                <p class="paragraph-prog">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, ipsam!</p>
+            </div>
+            <div class="content-prog">
+                <img src="../img/S_SDG_Icons-01-04.jpg" alt="" class="prog-img">
+                <h3 class="subtitle-prog">tema de programa</h3>
+                <p class="paragraph-prog">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi, ipsam!</p>
+            </div>
+        </div>
+    </section>
+
+    <div class="piel-pagina">
+    <div style="height: 150px; overflow: hidden;" ><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M-4.51,122.52 C171.56,192.40 296.84,22.14 501.12,133.36 L500.00,-0.00 L-0.00,-0.00 Z" style="stroke: none; fill: #fff;"></path></svg></div>
+        <div class="contenido">
+            <div class="caja">
+                <h3>Informacion</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur 
+                    pariatur dicta labore quidem error aut repellendus doloremque repudiandae ex, 
+                    similique cum, assumenda ut voluptatibus natus eaque officia. 
+                    Vitae, temporibus. Laboriosam!
+                </p>
+            </div>
+            <div class="caja">
+                <h3>Redes sociales</h3>
+                <a href="#">Facebook</a>
+                <a href="#">Instagram</a>
+                <a href="#">Github</a>
+            </div>
+            <div class="caja">
+                <h3>Contactos</h3>
+                <div class="info-user">
+                    <p>Nombre</p>
+                    <div class="info-contact">
+                        <i class="fa-solid fa-envelope"></i>
+                        <p>Correo</p>
+                    </div>
+                </div>
+                <div class="info-user">
+                    <p>Nombre</p>
+                    <div class="info-contact">
+                        <i class="fa-solid fa-envelope"></i>
+                        <p>Correo</p>
+                    </div>
+                </div>
+            </div>
+           
+        </div>
+        <h1 class="creditos">
+            &copy, copyright @ 2024
+        </h1>
+    </div>
+</body>
+</html>
