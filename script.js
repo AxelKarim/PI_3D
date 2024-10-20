7dbc3764789f68d17175a4b693f2aaaed3f363cf
DylanT10011 <147109877+DylanT10011@users.noreply.github.com>
2024-10-20 11:52:32 -0600

    se añadió el dashboard de beneficiarios.
    se añadio el html del dashboard junto a 4 archivos complementarios.

---
+++ script.js
@@ -0,0 +1,18 @@
+const ctx = document.getElementById('lineChart').getContext('2d');
+
+new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options:
+    {
+    responsive: true
+    
+  }
+});
